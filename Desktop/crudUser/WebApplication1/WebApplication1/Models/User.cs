9f166f8bc52ca2e54517482871d0cc1b1b3aed76
ghannoud chawki <ghannoudichawki63.com>
2024-07-12 02:05:34 +0200

    commit the crudUserapp

---
+++ Desktop/crudUser/WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string nom { get; set; } = string.Empty;
+        public string prenom { get; set; } = string.Empty ;
+
+    }
+}
---
+++ Desktop/crudUser/WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
+builder.Services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("UserContext")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowAllOrigins");
+
+app.Run();
