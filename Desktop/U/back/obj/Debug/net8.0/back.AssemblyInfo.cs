cf567f1898a968acd46456450b1d4a15a96dce2e
ghannoud chawki <ghannoudichawki63.com>
2024-07-26 13:04:12 +0200

    app running in linux

--- Desktop/U/back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ Desktop/U/back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d72fe829d741140e32f955005b9fc57da5f90ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7358e8121ffc18aa970ee23b49114d1698619d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
